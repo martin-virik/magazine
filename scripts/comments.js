9627d8ec719fe9c16e9be7aeedf8ef5561da920e
Martin Virik <MVirik@sk.ibm.com>
2017-02-03 11:23:41 +0100

    Form submitting, storing and getting comments from localstorage

--- scripts/comments.js
+++ scripts/comments.js
@@ -4,26 +4,59 @@
         init : function (){
             console.log('jQuery ready!');
             this.update();
+
+            $('form#comments-form').on('submit', this.onSubmit)
         },
 
         onSubmit: function(event) {
-            // stop submitting, validate and store
+            event.preventDefault();
+            var data = $( this ).serializeArray();
+
+            if (app.validateEmail(data[1].value)) {
+                app.hideEmailError();
+                app.storeComment(data);
+            } else {
+                app.showError();
+            }
+
+            return false;
         },
 
         validateEmail: function(email) {
             // check email
+            return true;
         },
 
-        showError: function() {
+        showEmailError: function() {
             
         },
 
-        storeComment: function() {
-            // save to locals
+        hideEmailError: function() {
+            
+        },
+
+        storeComment: function(data) {
+            var comments = this.getComments();
+
+            comments.push({
+                name: data[0].value,
+                email: data[1].value,
+                comment: data[2].value,
+            })
+
+            localStorage.comments = JSON.stringify(comments);
         },
 
         getComments: function() {
-            // get from locals
+            var comments;
+            try {
+                comments = JSON.parse(localStorage.comments);
+            } catch (error) {
+                console.error(error);
+                comments = [];
+            }
+
+            return comments;
         },
 
         update: function() {
